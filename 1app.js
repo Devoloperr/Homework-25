5d697740af7e64dfd10e4721ff92f9f60a5879b5
Devoloperr <mirkamilezizli085@gmail.com>
2024-07-17 20:20:42 +0400

    update

--- 1app.js
+++ 1app.js
@@ -1,10 +1,12 @@
 let user = prompt("Random bir sey daxil edin:");
+let num = 0;
 for (let i = 0; i < user.length; i++) {
-    let space = user.charAt();
-    if (space >= "0" && space <= "9") {
-        console.log("metnde reqem var.");
-    } else {
-        console.log("metnde reqem yoxdur.");
-
+    if (user[i] == user.match(/[0-9]/)) {
+        num++;
     }
+}
+if (num == 0) {
+    console.log("metnde reqem yoxdur.");
+} else {
+    console.log("metnde reqem var.");
 }
